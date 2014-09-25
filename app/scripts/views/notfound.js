42808deac803d924b19bd6130112ddb598a5cabf
Matt Cain <matt@cainmedia.com>
2014-09-25 11:56:01 +0100

    Added not found view and template

---
+++ app/scripts/views/notfound.js
@@ -0,0 +1,13 @@
+var App = App || {};
+App.classes = App.classes || {};
+
+// 404 view just shows a 404 message, doesn't need to extend base unless you
+// want it to do more than that (unlikely)
+App.classes.NotfoundView = Backbone.View.extend({
+	el: '#content',
+	template: App.templates.notfound,
+	initialize: function(data) {
+		'use strict';
+		this.$el.html(this.template(data));
+	}
+});
